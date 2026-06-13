520c245f9878a7e2c63a4c60a05a47ec819f7562
SAHADHERE <mohammedsahad620@gmail.com>
2026-06-13 20:43:38 +0530

    Enable profile photo on mobile screens inside About section

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -311,40 +311,38 @@ export default function About() {
           </motion.div>
 
           {/* IMAGE */}
-          {!isMobile && (
-            <motion.div
-              variants={slideLeft}
-              initial="hidden"
-              whileInView="show"
-              viewport={{ once: false }}
+          <motion.div
+            variants={slideLeft}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false }}
+            style={{
+              width: isMobile ? "100%" : "48%",
+              display: "flex",
+              justifyContent: isMobile ? "center" : "flex-end",
+            }}
+          >
+            <div
               style={{
-                width: "48%",
-                display: "flex",
-                justifyContent: "flex-end",
+                padding: 12,
+                borderRadius: "50%",
+                border: "1px solid var(--border)",
+                transform: isMobile ? "none" : "translateX(-80px)",
               }}
             >
-              <div
+              <img
+                src={portfolioConfig.personalInfo.profileImage}
+                alt="Profile"
                 style={{
-                  padding: 12,
+                  width: isMobile ? 180 : 240,
+                  height: isMobile ? 180 : 240,
                   borderRadius: "50%",
-                  border: "1px solid var(--border)",
-                  transform: "translateX(-80px)",
+                  objectFit: "cover",
+                  display: "block",
                 }}
-              >
-                <img
-                  src={portfolioConfig.personalInfo.profileImage}
-                  alt="Profile"
-                  style={{
-                    width: 240,
-                    height: 240,
-                    borderRadius: "50%",
-                    objectFit: "cover",
-                    display: "block",
-                  }}
-                />
-              </div>
-            </motion.div>
-          )}
+              />
+            </div>
+          </motion.div>
         </div>
 
         {/* CARDS */}
